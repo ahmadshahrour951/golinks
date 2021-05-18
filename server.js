30251995f11d479598addf10057a99614ab5cc50
shahrour <ahmadshahrour951@gmail.com>
2021-05-18 17:23:20 -0500

    Create server.js

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const logger = require('morgan');
+const cors  = require('cors')
+
+const routes = require('./routes')
+
+const app = express();
+const PORT = process.env.PORT || 8000;
+
+app.use(logger('combined'));
+app.use(express.json());
+app.use(cors('*'));
+
+app.use('/api/v1', routes)
+
+app
+  .listen(PORT, () => {
+    console.log(`Server started on port ${PORT}`)
+  })
+
+module.exports = app;
