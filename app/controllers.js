14294d4d6e8ed94e55483eac367536307350c59e
shahrour <ahmadshahrour951@gmail.com>
2021-05-19 10:03:35 -0500

    init getUserStats

--- app/controllers.js
+++ app/controllers.js
@@ -1,1 +1,20 @@
-module.exports = {};
+const axios = require('axios');
+
+async function getUserStats(req, res, next) {
+  let outputObj = {
+    repos_count: 0,
+    stargazers_count: 0,
+    forks_count: 0,
+    repos_avg_size: 0,
+    repos_langs: [],
+  };
+
+  try {
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+module.exports = {
+  getUserStats,
+};
