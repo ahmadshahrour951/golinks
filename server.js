3e93cd4f5782b469eed593ba6e4840e94ece9be7
shahrour <ahmadshahrour951@gmail.com>
2021-05-19 10:02:28 -0500

    require dotenv for variables in dev

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv-safe').config();
+}
 const express = require('express');
 const logger = require('morgan');
-const cors  = require('cors')
+const cors = require('cors');
 
-const routes = require('./app/routes')
+const routes = require('./app/routes');
 
 const app = express();
 const PORT = process.env.PORT || 8000;
@@ -11,11 +14,10 @@ app.use(logger('combined'));
 app.use(express.json());
 app.use(cors('*'));
 
-app.use('/api/v1', routes)
+app.use('/api/v1', routes);
 
-app
-  .listen(PORT, () => {
-    console.log(`Server started on port ${PORT}`)
-  })
+app.listen(PORT, () => {
+  console.log(`Server started on port ${PORT}`);
+});
 
 module.exports = app;
