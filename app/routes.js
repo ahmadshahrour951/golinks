051c3ff149e527a35b8a869b4b0b1876b63166bf
shahrour <ahmadshahrour951@gmail.com>
2021-05-19 16:36:14 -0500

    Add error handling catches

--- app/routes.js
+++ app/routes.js
@@ -1,16 +1,25 @@
 const express = require('express');
 const router = express.Router();
 
-const controllers = require('./controllers')
+const controllers = require('./controllers');
 
-router.get('/:username', controllers.getUserStats)
+router.get('/:username', controllers.getUserStats);
 
-// Fall back for error handling
+/////////////////////////////////////////////////////////////////////////////////////////
+// Error page for error handling
+/////////////////////////////////////////////////////////////////////////////////////////
 router.use((error, req, res, next) => {
   const status = error.statusCode || 500;
   const message = error.message;
   const data = error.data;
   res.status(status).json({ message: message, data: data });
+});
+
+/////////////////////////////////////////////////////////////////////////////////////////
+// If no explicit error and route requested not found
+/////////////////////////////////////////////////////////////////////////////////////////
+router.use((req, res, next) => {
+  res.status(404).json({ message: 'API endpoint invalid', data: {} });
 });
 
 module.exports = router;
