4de6976e4a53e647a4de42c3348f659a3f38c0ef
shahrour <ahmadshahrour951@gmail.com>
2021-05-20 13:18:09 -0500

    small changes to server

--- server.js
+++ server.js
@@ -5,16 +5,29 @@ const cors = require('cors');
 const routes = require('./app/routes');
 
 const app = express();
-const PORT = process.env.PORT || 8000;
+const PORT = process.env.PORT || 5000;
 
+// third party packages middleware
 app.use(logger('combined'));
 app.use(express.json());
 app.use(cors('*'));
 
 app.use('/', routes);
 
-app.listen(PORT, () => {
-  console.log(`Server started on port ${PORT}`);
-});
+// Server starts here
+app
+  .listen(PORT, () => {
+    console.log(`Server started on port ${PORT}`);
+  })
+  .on('error', function (err) {
+    if (err.errno === 'EADDRINUSE') {
+      console.log(
+        `----- Port ${PORT} is busy -----`
+      );
+    } else {
+      console.log(err);
+    }
+  });
+
 
 module.exports = app;
